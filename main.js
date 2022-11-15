23d2f49734b48dd9c449790f85d2ba26bede6967
Nils <olivernils8@gmail.com>
2022-11-15 01:19:15 +0100

    Save item list as JSON when the user exits

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const fs = require('fs')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     autoHideMenuBar: true,
+    webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
   })
 
   win.loadFile('app/index.html')
+
 }
 
 app.whenReady().then(() => {
   createWindow()
+})
+
+function saveToJson(itemToSave){
+  let json = JSON.stringify(itemToSave)
+  fs.writeFile('items.json', json, 'utf8', function(){})
+}
+
+ipcMain.on('window:close', (event, item) => {
+  saveToJson(item)
 })
