33e7ac676712fa49710871cacb32dd58403aa512
Nils <olivernils8@gmail.com>
2022-11-18 22:04:15 +0100

    Add comments

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const createWindow = () => {
     width: 800,
     height: 600,
     autoHideMenuBar: true,
-    webPreferences: {
+    webPreferences: { // Enable the use of 'require' in HTML
             nodeIntegration: true,
             contextIsolation: false,
         }
@@ -18,7 +18,7 @@ const createWindow = () => {
 
   win.loadFile('app/index.html')
 
-  fs.readFile('items.json', 'utf8', function(err, data){
+  fs.readFile('items.json', 'utf8', function(err, data){ // Read the JSON file and send the item list.
     data = JSON.parse(data)
     win.webContents.send('window:open', data.items)
   })
@@ -29,11 +29,11 @@ app.whenReady().then(() => {
   createWindow()
 })
 
-function saveItemsToJson(itemToSave){
+function saveItemsToJson(itemToSave){ // Write item into JSON file.
   fs.writeFile('items.json', JSON.stringify(itemToSave), 'utf8', function(){})
 }
 
-ipcMain.on('window:close', (event, item) => {
+ipcMain.on('window:close', (event, item) => { // Update the local dict and save it to file.
   for (let i = 0; i < item.length; i++){
     dict.items.push(item[i])
   }
