d8118c1d7b479c6319d7627c2c25ff70e1af44ae
Nils <olivernils8@gmail.com>
2022-12-12 23:38:05 +0100

    Fixed cant change variable defined inside function using fs.readFileSync

--- Shopping list v.2/app/index.js
+++ Shopping list v.2/app/index.js
@@ -1,15 +1,7 @@
 const fs = require("fs")
 
-let template = "gay"
-
-fs.readFile("app/items.json", "utf-8", (err, contents) => {getTemplate(err, contents)})
-
-function getTemplate(err, contents){
-	template = contents
-}
+const template = fs.readFileSync('app/items.json', {encoding:'utf8', flag:'r'}) // Get template from items.json
 
 function generateForm() {
 	let form = document.querySelector(".add-item-form")
 }
-
-console.log(template)
