be7cdc8e6a6dbc1cbfbfdd9f4d7c5ac8a68cec4a
Nils <olivernils8@gmail.com>
2022-12-15 23:42:11 +0100

    [ADD] Added item addition functionality

--- Shopping list v.2/app/index.js
+++ Shopping list v.2/app/index.js
@@ -1,10 +1,17 @@
 const fs = require("fs")
 
-const items = JSON.parse(fs.readFileSync('app/items.json', {encoding:'utf8', flag:'r'})) // Get template from items.json
+const json = JSON.parse(fs.readFileSync('app/items.json', {encoding:'utf8', flag:'r'})) // Get template from items.json
 
-const template = items.template 
+const template = json.template
+
+const items = json.items
 	
 const form = document.querySelector("#add-item-form")
+
+const itemList = []
+
+const itemDiv = document.querySelector("#item-div")
+console.log(itemDiv)
 
 function makeInput(params){
 	let element = document.createElement("input")
@@ -16,17 +23,17 @@ function makeInput(params){
 	
 }
 
-function makeButtonInput(){
+function makeButtonInput(){ // Add button to form
 	let button = document.createElement("button")
 	button.innerHTML = "Add Item"
-	button.onclick = "this.form.submit"
+	button.onclick = function() {addItem()}
 	form.appendChild(button)
 }
 
 function generateForm() {
 	for (let i = 0; i < Object.keys(template).length; i++){
 		let key = Object.keys(template)[i]
-		if (key[key.length-1] != "_"){
+		if (key[key.length-1] != "_"){ // Only add items which names do not end with "_"
 			let params = {
 				"name" : key,
 				"type" : template[key]
@@ -37,4 +44,67 @@ function generateForm() {
 	makeButtonInput()
 }
 
+function loadItems(){
+	for (let i = 0; i < items.length; i++) {
+		let item = items[i]
+		itemList.push(JSON.parse(item))
+	}
+}
+
+function addItem(){
+	let item = {}
+	for (let i = 0; i < form.children.length; i++){
+		let input = form.children[i]
+		item[input.name] = input.value
+	}
+	itemList.push(item)
+	updateItemDiv()
+}
+
+function clearChildren(element){
+	let last = element.lastElementChild
+
+	while (last){
+		element.removeChild(last)
+		last = element.lastElementChild
+	}
+}
+
+function updateItemDiv() {
+	clearChildren(itemDiv)
+	for (let i = 0; i < itemList.length; i++){
+		let item = itemList[i]
+
+		// Convert object to HTML
+
+		let element = document.createElement("div")
+		let heading = document.createElement("h3")
+		let amount = document.createElement("p")
+		let notes = document.createElement("p")
+
+		element.classList.add("item-div")
+		heading.classList.add("item-header")
+		amount.classList.add("item-amount")
+		notes.classList.add("item-notes")
+
+		heading.innerHTML = item.name
+		amount.innerHTML = item.amount
+		notes.innerHTML = item.notes
+
+		element.appendChild(heading)
+		element.appendChild(amount)
+		element.appendChild(notes)
+
+		itemDiv.appendChild(element)
+	}
+}
 generateForm()
+loadItems() // Load items from JSON
+
+window.onbeforeunload = function() {
+	for (let i = 0; i < itemList.length; i++){
+		json.items.push(item)
+	}
+	fs.writeFile('app/items.json', JSON.stringify(json), (err) => {console.log(err)})
+	return false
+}
