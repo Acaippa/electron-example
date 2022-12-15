41d86a04643a24b1358de94e8360d37d178d4966
Nils <olivernils@gmail.com>
2022-12-15 14:01:08 +0100

    [ADD] Added makeForm function

--- Shopping list v.2/app/index.js
+++ Shopping list v.2/app/index.js
@@ -1,7 +1,40 @@
 const fs = require("fs")
 
-const template = fs.readFileSync('app/items.json', {encoding:'utf8', flag:'r'}) // Get template from items.json
+const items = JSON.parse(fs.readFileSync('app/items.json', {encoding:'utf8', flag:'r'})) // Get template from items.json
+
+const template = items.template 
+	
+const form = document.querySelector("#add-item-form")
+
+function makeInput(params){
+	let element = document.createElement("input")
+	element.type = params.type
+	element.name = params.name
+	element.placeholder = params.name
+	element.classList.add("bg-2")
+	form.appendChild(element)
+	
+}
+
+function makeButtonInput(){
+	let button = document.createElement("button")
+	button.innerHTML = "Add Item"
+	button.onclick = "this.form.submit"
+	form.appendChild(button)
+}
 
 function generateForm() {
-	let form = document.querySelector(".add-item-form")
+	for (let i = 0; i < Object.keys(template).length; i++){
+		let key = Object.keys(template)[i]
+		if (key[key.length-1] != "_"){
+			let params = {
+				"name" : key,
+				"type" : template[key]
+			}
+			makeInput(params)
+		}
+	}
+	makeButtonInput()
 }
+
+generateForm()
